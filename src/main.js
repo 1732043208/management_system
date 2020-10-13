0151dfad0f90c7dcf7b4bb27f66124db56cdd136
黄伟杰 <1732043208@qq.com>
2020-10-13 17:29:19 +0800

    修改element UI导入方式(按需导入)

--- src/main.js
+++ src/main.js
@@ -2,18 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
-
+import '../src/plugins/element'
 
 Vue.config.productionTip = false
 
-//Element的完整引入
-Vue.use(ElementUI);
 
 new Vue({
   router,

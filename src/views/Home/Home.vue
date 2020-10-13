34a362c2d12e92aad02681ff9ca84d976c88b3dd
黄伟杰 <1732043208@qq.com>
2020-10-13 15:17:08 +0800

    Home页面的头部布局

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,15 +1,21 @@
 <template>
-        <el-container class="home-container">
-<!--            头部区域-->
-            <el-header>Header  <el-button type="info" @click="loginout">退出按钮</el-button></el-header>
-<!--            页面主体区域-->
-            <el-container>
-<!--                侧边栏-->
-                <el-aside width="200px">Aside</el-aside>
-<!--                右侧内容主体-->
-                <el-main>Main</el-main>
-            </el-container>
+    <el-container class="home-container">
+        <!--            头部区域-->
+        <el-header>
+            <div>
+                <img src="../../assets/home/heima.png"/>
+                <span>Joker商城后台管理系统</span>
+            </div>
+            <el-button type="info" @click="loginout">退出按钮</el-button>
+        </el-header>
+        <!--            页面主体区域-->
+        <el-container>
+            <!--                侧边栏-->
+            <el-aside width="200px">Aside</el-aside>
+            <!--                右侧内容主体-->
+            <el-main>Main</el-main>
         </el-container>
+    </el-container>
 
 </template>
 
@@ -19,30 +25,46 @@
     export default {
         name: 'Home',
         components: {},
-        methods:{
-            loginout(){
+        methods: {
+            loginout() {
                 window.sessionStorage.clear();
                 this.$router.push('/Login');
-                    this.$message({
-                        showClose: true,
-                        message: '已退出登录！'
-                    });
+                this.$message({
+                    showClose: true,
+                    message: '已退出登录！'
+                });
 
             }
         }
     }
 </script>
 <style lang="less" scoped>
-    .el-header{
+    .el-header {
         background-color: #373d41;
+        display: flex;
+        justify-content: space-between;
+        padding-left: 0;
+        align-items: center;
+        color: #fff;
+        font-size: 20px;
+     > div{
+         display: flex;
+         align-items: center;
+         span{
+             margin-left: 15px;
+         }
+     }
     }
-    .el-aside{
+
+    .el-aside {
         background-color: #333744;
     }
-    .el-main{
+
+    .el-main {
         background-color: #EAEDF1;
     }
-    .home-container{
+
+    .home-container {
         height: 100%;
     }
 </style>

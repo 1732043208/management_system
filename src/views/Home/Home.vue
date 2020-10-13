c720cc3050a6936abe4c76cf257a0415e556f7de
黄伟杰 <1732043208@qq.com>
2020-10-13 14:35:17 +0800

    实现退出登录功能

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
         <div>我是首页</div>
+        <el-button type="info" @click="loginout">退出按钮</el-button>
     </div>
 </template>
 
@@ -9,6 +10,17 @@
 
     export default {
         name: 'Home',
-        components: {}
+        components: {},
+        methods:{
+            loginout(){
+                window.sessionStorage.clear()
+                this.$router.push('./login')
+                    this.$message({
+                        showClose: true,
+                        message: '已退出登录！'
+                    });
+
+            }
+        }
     }
 </script>

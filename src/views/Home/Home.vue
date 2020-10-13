761187bea0c80b9c48e88bfcfd7c67ba103701ae
黄伟杰 <1732043208@qq.com>
2020-10-13 15:37:59 +0800

    Home页面的侧边栏布局

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -11,7 +11,35 @@
         <!--            页面主体区域-->
         <el-container>
             <!--                侧边栏-->
-            <el-aside width="200px">Aside</el-aside>
+            <el-aside width="200px">
+                <!--                侧边栏菜单区-->
+                <el-menu
+                        background-color="#333744"
+                        text-color="#fff"
+                        active-text-color="#ffd04b">
+                    <!--                    一级菜单-->
+                    <el-submenu index="1">
+                        <!--                        一级菜单的模板区-->
+                        <template slot="title">
+                            <!--                            图标-->
+                            <i class="el-icon-location"></i>
+                            <!--                            文本-->
+                            <span>导航一</span>
+                        </template>
+
+                        <!--                        二级菜单-->
+                        <el-menu-item index="1-4-1">
+                            <template slot="title">
+                                <!--                            图标-->
+                                <i class="el-icon-location"></i>
+                                <!--                            文本-->
+                                <span>导航一</span>
+                            </template>
+                        </el-menu-item>
+                    </el-submenu>
+
+                </el-menu>
+            </el-aside>
             <!--                右侧内容主体-->
             <el-main>Main</el-main>
         </el-container>
@@ -47,13 +75,15 @@
         align-items: center;
         color: #fff;
         font-size: 20px;
-     > div{
-         display: flex;
-         align-items: center;
-         span{
-             margin-left: 15px;
-         }
-     }
+
+        > div {
+            display: flex;
+            align-items: center;
+
+            span {
+                margin-left: 15px;
+            }
+        }
     }
 
     .el-aside {

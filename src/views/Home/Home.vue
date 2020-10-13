f9b3a73eb77e5ea16659e89e783374d35f6470a0
黄伟杰 <1732043208@qq.com>
2020-10-13 15:07:25 +0800

    Home页面的整体布局

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="home">
-        <div>我是首页</div>
-        <el-button type="info" @click="loginout">退出按钮</el-button>
-    </div>
+        <el-container class="home-container">
+<!--            头部区域-->
+            <el-header>Header  <el-button type="info" @click="loginout">退出按钮</el-button></el-header>
+<!--            页面主体区域-->
+            <el-container>
+<!--                侧边栏-->
+                <el-aside width="200px">Aside</el-aside>
+<!--                右侧内容主体-->
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+
 </template>
 
 <script>
@@ -24,3 +32,17 @@
         }
     }
 </script>
+<style lang="less" scoped>
+    .el-header{
+        background-color: #373d41;
+    }
+    .el-aside{
+        background-color: #333744;
+    }
+    .el-main{
+        background-color: #EAEDF1;
+    }
+    .home-container{
+        height: 100%;
+    }
+</style>

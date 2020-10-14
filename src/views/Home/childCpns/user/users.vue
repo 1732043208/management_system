6a6d50064e7911ee3d491f41a934d4aeb86d9108
黄伟杰 <1732043208@qq.com>
2020-10-14 14:11:01 +0800

    home 给表单添加分页效果

--- src/views/Home/childCpns/user/users.vue
+++ src/views/Home/childCpns/user/users.vue
@@ -2,7 +2,7 @@
     <div>
         <!--        面包屑导航区域-->
         <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item :to="{ path: '/home' }" >首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
             <el-breadcrumb-item>用户管理</el-breadcrumb-item>
             <el-breadcrumb-item>用户列表</el-breadcrumb-item>
         </el-breadcrumb>
@@ -28,31 +28,55 @@
                     stripe
                     style="width: 100%"
                     border>
-                <el-table-column
-                        prop="username"
-                        label="姓名"
-                        width="180">
+                <el-table-column type="index"/>
+
+                <el-table-column prop="username" label="姓名"/>
+
+                <el-table-column prop="email" label="邮箱"/>
+
+                <el-table-column prop="mobile" label="电话"/>
+
+                <el-table-column prop="role_name" label="角色"/>
+
+                <el-table-column label="状态">
+                    <template slot-scope="scope">
+                        <el-switch
+                                v-model="scope.row.mg_state">
+                        </el-switch>
+                    </template>
                 </el-table-column>
-                <el-table-column
-                        prop="email"
-                        label="邮箱">
+
+                <el-table-column label="操作" width="180">
+                    <template slot-scope="scope">
+                        <!--                        修改按钮-->
+                        <el-tooltip class="item" effect="dark" content="Top Center 提示文字" placement="top"
+                                    :open-delay=500 :enterable="false">
+                            <el-button type="primary" icon="el-icon-edit" size="mini"/>
+                        </el-tooltip><!--                        删除按钮-->
+                        <el-tooltip effect="dark" content="Top Center 提示文字" placement="top" :open-delay=500
+                                    :enterable="false">
+                            <el-button type="danger" icon="el-icon-delete" size="mini"/>
+                        </el-tooltip>
+                        <!--                        分配角色按钮-->
+                        <el-tooltip effect="dark" content="分配角色" placement="top" :open-delay=500 :enterable="false">
+                            <el-button type="warning" icon="el-icon-setting" size="mini"/>
+                        </el-tooltip>
+
+
+                    </template>
                 </el-table-column>
-                <el-table-column
-                        prop="mobile"
-                        label="电话">
-                </el-table-column>
-                <el-table-column
-                        prop="role_name"
-                        label="角色">
-                </el-table-column>
-                <el-table-column
-                        prop="mg_state"
-                        label="状态">
-                </el-table-column>
-                <el-table-column
-                        label="操作">
-                </el-table-column>
+
             </el-table>
+
+            <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="queryInfo.pagenum"
+                    :page-sizes="[1, 2, 5, 10]"
+                    :page-size="queryInfo.pagesize"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="total">
+            </el-pagination>
         </el-card>
 
 
@@ -68,8 +92,11 @@
             return {
                 //获取用户列表的参数对象
                 queryInfo: {
+                    //查询参数
                     query: '',
+                    //当前的页数
                     pagenum: 1,
+                    // 当前每页显示多少条数据
                     pagesize: 2
                 },
                 usersList: [],
@@ -80,6 +107,7 @@
             getUsersFunc() {
                 getUsers(this.queryInfo.query, this.queryInfo.pagenum, this.queryInfo.pagesize,)
                     .then(res => {
+                        console.log(res);
                         this.usersList = res.data.users;
                         this.total = res.data.total;
                     })
@@ -87,6 +115,18 @@
                         console.log(err);
                     })
             },
+            //监听pageSize改变事件
+            handleSizeChange(newSize) {
+                // console.log(newSize);
+                this.queryInfo.pagesize = newSize;
+                this.getUsersFunc()
+            },
+            // 监听页码值改变的事件
+            handleCurrentChange(newPage) {
+                // console.log(newPage);
+                this.queryInfo.pagenum = newPage;
+                this.getUsersFunc()
+            }
         },
         created() {
             this.getUsersFunc()

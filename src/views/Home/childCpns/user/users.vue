04d10e54a46cb453a74ec36554a73ad672897a2a
黄伟杰 <1732043208@qq.com>
2020-10-14 18:07:17 +0800

    home 实现添加用户功能

--- src/views/Home/childCpns/user/users.vue
+++ src/views/Home/childCpns/user/users.vue
@@ -17,7 +17,7 @@
                     </el-input>
                 </el-col>
                 <el-col :span="4">
-                    <el-button type="primary">添加用户</el-button>
+                    <el-button type="primary" @click="addDialogVisible=true">添加用户</el-button>
                 </el-col>
             </el-row>
 
@@ -79,6 +79,19 @@
             </el-pagination>
         </el-card>
 
+        <!--        添加用户的对话框-->
+        <el-dialog
+                title="提示"
+                :visible.sync="addDialogVisible"
+                width="30%">
+            <!--            内容主体区-->
+            <span>这是一段信息</span>
+            <!--            底部区域-->
+            <span slot="footer" class="dialog-footer">
+    <el-button @click="addDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+  </span>
+        </el-dialog>
 
     </div>
 </template>
@@ -100,7 +113,9 @@
                     pagesize: 2
                 },
                 usersList: [],
-                total: 0
+                total: 0,
+                //控制添加用户对话框的显示与隐藏
+                addDialogVisible: false
             }
         },
         methods: {
